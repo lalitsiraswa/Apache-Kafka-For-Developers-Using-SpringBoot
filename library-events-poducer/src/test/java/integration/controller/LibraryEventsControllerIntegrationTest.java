efa0b05730b24367d3c839d7d15a4723d48353fb
lalitsiraswa <lalitsiraswa1999@gmail.com>
2023-06-08 21:48:43 +0530

    Integrate Kafka Consumer in JUnit and Consume the record

--- library-events-poducer/src/test/java/integration/controller/LibraryEventsControllerIntegrationTest.java
+++ library-events-poducer/src/test/java/integration/controller/LibraryEventsControllerIntegrationTest.java
@@ -1,16 +1,31 @@
 package controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.learnkafka.LibraryEventsPoducerApplication;
 import com.learnkafka.domain.Book;
 import com.learnkafka.domain.LibraryEvent;
 import com.learnkafka.domain.LibraryEventType;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.test.context.TestPropertySource;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -26,9 +41,30 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
         "spring.kafka.admin.properties.bootstrap.servers=${spring.embedded.kafka.brokers}"})
 public class LibraryEventsControllerIntegrationTest {
     @Autowired
-    TestRestTemplate testRestTemplate;
+    private TestRestTemplate testRestTemplate;
+    @Autowired
+    private ObjectMapper MAPPER;
+    @Autowired
+    private EmbeddedKafkaBroker embeddedKafkaBroker;
+    private Consumer<Integer, String> consumer;
+
+//    Before Each Test-Case
+    @BeforeEach
+    void setUp() {
+        Map<String, Object> configs = new HashMap<>(KafkaTestUtils.consumerProps("group1", "true", embeddedKafkaBroker));
+        consumer = new DefaultKafkaConsumerFactory<>(configs, new IntegerDeserializer(), new StringDeserializer()).createConsumer();
+        embeddedKafkaBroker.consumeFromAllEmbeddedTopics(consumer);
+    }
+//    After Each Test-Case
+    @AfterEach
+    void tearDown() {
+        consumer.close();
+    }
+
     @Test
-    void postLibraryEvent(){
+    @Timeout(5) // We are using @Timeout(Default unit is Seconds) instead of Thread.sleep(5000);
+    // @Timeout(5) :- This test-case is going to wait for 5-Seconds before it completes execution.
+    void postLibraryEvent() throws JsonProcessingException, InterruptedException {
 //        given
         Book book = Book.builder()
                 .bookId(201)
@@ -43,9 +79,15 @@ public class LibraryEventsControllerIntegrationTest {
         HttpHeaders headers = new HttpHeaders();
         headers.set("content-type", MediaType.APPLICATION_JSON.toString());
         HttpEntity<LibraryEvent> request = new HttpEntity<>(libraryEvent, headers);
+        String expectedRecord = MAPPER.writeValueAsString(libraryEvent);
 //        when
         ResponseEntity<LibraryEvent> responseEntity = testRestTemplate.exchange("/v1/libraryevent", HttpMethod.POST, request, LibraryEvent.class);
 //        then
         assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+//        The KafkaTestUtils.getSingleRecord() Call has Asynchronous Behaviour
+        ConsumerRecord<Integer, String> consumerRecord = KafkaTestUtils.getSingleRecord(consumer, "library-events");
+        // Thread.sleep(5000); // Don't use Thread.sleep(), instead use @Timeout(Default Unit is Seconds)
+        String value = consumerRecord.value();
+        assertEquals(expectedRecord, value);
     }
 }
