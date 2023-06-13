864669f714bafe910c30780cae52ef78e86ab8f1
lalitsiraswa <lalitsiraswa1999@gmail.com>
2023-06-14 00:12:10 +0530

    Create the PUT endpoint - 'v1/libraryEvent'

--- library-events-poducer/src/main/java/com/learnkafka/controller/LibraryEventsController.java
+++ library-events-poducer/src/main/java/com/learnkafka/controller/LibraryEventsController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,4 +39,16 @@ public class LibraryEventsController {
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
 //    TODO: PUT Method
+    @PutMapping("/v1/libraryevent") // @Valid validate all the Constraints, that we provide in 'LibraryEvent.java' Class
+    public ResponseEntity<?> putLibraryEvent(@RequestBody @Valid LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException, TimeoutException {
+    //        TODO: Invoke Kafka Producer
+        log.info("before sendLibraryEvent");
+        if(libraryEvent.getLibraryEventId() == null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Please pass the LibraryEventId.");
+        }
+        libraryEvent.setLibraryEventType(LibraryEventType.UPDATE);
+        libraryEventProducer.sendLibraryEvent_Approach2(libraryEvent); // This one is the Asynchronous Call
+        log.info("after sendLibraryEvent");
+        return ResponseEntity.status(HttpStatus.OK).body(libraryEvent);
+    }
 }
